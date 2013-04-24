91836ecdc848891a7f10da3d8127ba2051b3fe61
AustinBGibbons <gibbons4@stanford.edu>
2013-04-23 20:35:00 -0700

    SimpleVector rewritten in proposed syntax

--- src/examples/SimpleVectorDSL.scala
+++ src/examples/SimpleVectorDSL.scala
@@ -49,49 +49,67 @@ trait SimpleVectorDSL extends ForgeApplication with ScalaOps {
     // does wrapping the op arguments in specifiers make things any clearer?    
     // val vnew = op (Vector) ("apply", methodTpe(static), tpePars(T), args(MInt), retTpe(Vector), codegenerated, effect = mutable)
         
-    val vnew = op (Vector) ("apply", static, List(T), List(MInt), Vector, allocates(vdata, 
+    val vnew = op (Vector) ("apply", static, List(T), List(MInt), Vector, effect = mutable)
+    allocates (vnew) (vdata, {
       ("_length" -> quotedArg(0)), ("_data" -> ("darray_new[T]("+quotedArg(0)+")"))
-    ), effect = mutable)
+    })
       
-    val vapply = op (Vector) ("apply", infix, List(T), List(Vector,MInt), T, composite(T, {
+    val vapply = op (Vector) ("apply", infix, List(T), List(Vector,MInt), T)
+    composite (vapply) (T, {
       "vector_raw_data("+quotedArg(0)+").apply("+quotedArg(1)+")"
-    }))
-    val vupdate = op (Vector) ("update", infix, List(T), List(Vector,MInt,T), MUnit, composite(MUnit, {
+    })
+
+    val vupdate = op (Vector) ("update", infix, List(T), List(Vector,MInt,T), MUnit, effect = write(0))
+    composite (vupdate) (MUnit, {
       "vector_raw_data("+quotedArg(0)+").update("+quotedArg(1)+","+quotedArg(2)+")"
-    }), effect = write(0))
+    })
     
-    val vtimesScalar = op (Vector) ("*", infix, List(T withBound TNumeric), List(Vector,T), Vector, map((T,T,Vector), 0, "e => e*"+quotedArg(1)))
-    val vfoo = op (Vector) ("foo", infix, List(T), List(Vector,T), tpeInst(Vector,List(MDouble)), map((T,MDouble,Vector), 0, "e => unit(0.0)")) // problem: primitive lifting isn't in scope in the ops
+    val vtimesScalar = op (Vector) ("*", infix, List(T withBound TNumeric), List(Vector,T), Vector)
+    map (vtimesScalar) ((T,T,Vector), 0, "e => e*"+quotedArg(1))
+
+    val vfoo = op (Vector) ("foo", infix, List(T), List(Vector,T), tpeInst(Vector,List(MDouble))) 
+    map (vfoo) ((T,MDouble,Vector), 0, "e => unit(0.0)") // problem: primitive lifting isn't in scope in the ops
      
     // uses a function arg inside a delite op
-    val vbar = op (Vector) ("bar", infix, List(T), List(Vector,("f", MFunction(List(("x",T)),T))), Vector, map((T,T,Vector), 0, "e => "+quotedArg("f")+"(e)")) 
+    val vbar = op (Vector) ("bar", infix, List(T), List(Vector,("f", MFunction(List(("x",T)),T))), Vector) 
+    map((T,T,Vector), 0, "e => "+quotedArg("f")+"(e)")
 
     // generic map, reduce functions for vector
     val mapper = MFunction(List(("x",T)),T)  // better syntax for functions would be nice
     val reducer = MFunction(List(("a",T),("b",T)),T)      
-    val vmap = op (Vector) ("map", infix, List(T), List(Vector,mapper), Vector, map((T,T,Vector), 0, "e => "+quotedArg(1)+"(e)"))    
-    val vreduce = op (Vector) ("reduce", infix, List(T withBound TNumeric), List(Vector,reducer), T, reduce((T,Vector), 0, lookup("Numeric","zero").get, "(a,b) => "+quotedArg(1)+"(a,b)"))
+    val vmap = op (Vector) ("map", infix, List(T), List(Vector,mapper), Vector) 
+    map (vmap) ((T,T,Vector), 0, "e => "+quotedArg(1)+"(e)")
+    val vreduce = op (Vector) ("reduce", infix, List(T withBound TNumeric), List(Vector,reducer), T) 
+    reduce (vreduce) ((T,Vector), 0, lookup("Numeric","zero").get, "(a,b) => "+quotedArg(1)+"(a,b)")
   
     val cond = MFunction(List(("x",T)),MBoolean)
-    val vfilter = op (Vector) ("filter", infix, List(T), List(Vector,cond), Vector, filter((T,T,Vector), 0, "e => " + quotedArg(1) + "(e)", "e => e"))
+    val vfilter = op (Vector) ("filter", infix, List(T), List(Vector,cond), Vector) 
+    filter (vfilter) ((T,T,Vector), 0, "e => " + quotedArg(1) + "(e)", "e => e")
     
     // a composite op
-    val vmapreduce = op (Vector) ("mapreduce", infix, List(T withBound TNumeric), List(Vector,mapper,reducer), T, composite(Vector, {
+    val vmapreduce = op (Vector) ("mapreduce", infix, List(T withBound TNumeric), List(Vector,mapper,reducer), T) 
+    composite (vmapreduce) (Vector, {
       quotedArg(0)+".map("+quotedArg(1)+").reduce("+quotedArg(2)+")"
-    }))
+    })
     
-    val vbasic = op (Vector) ("basic", infix, List(), List(MInt, ("y", MInt, "1"), ("z", MInt, "1")), MInt, codegenerated)
+    val vbasic = op (Vector) ("basic", infix, List(), List(MInt, ("y", MInt, "1"), ("z", MInt, "1")), MInt)
     codegen (vbasic) ($cala, quotedArg("y")+ "+3+" + quotedArg("z"))
  
-    val vset = op (Vector) ("set", infix, List(T withBound TNumeric), List(Vector, ("x", MInt), ("z", MInt, "3")), tpeInst(Vector, List(MInt)), map ((T,MInt,Vector), 0, "y  => "+quotedArg("x") + " + " + quotedArg("z")))
+    val vset = op (Vector) ("set", infix, List(T withBound TNumeric), List(Vector, ("x", MInt), ("z", MInt, "3")), tpeInst(Vector, List(MInt))) 
+    map (vset) ((T,MInt,Vector), 0, "y  => "+quotedArg("x") + " + " + quotedArg("z"))
 
-    val vplus = op (Vector) ("+", infix, List(T withBound TNumeric), List(Vector,Vector), Vector, zip((T,T,T,Vector), (0,1), "(a,b) => a+b"))
+    val vplus = op (Vector) ("+", infix, List(T withBound TNumeric), List(Vector,Vector), Vector) 
+    zip (vplus) ((T,T,T,Vector), (0,1), "(a,b) => a+b")
     
-    val vsum = op (Vector) ("sum", infix, List(T withBound TNumeric), List(Vector), T, reduce((T,Vector), 0, lookup("Numeric","zero").get, "(a,b) => a+b"))
+    val vsum = op (Vector) ("sum", infix, List(T withBound TNumeric), List(Vector), T) 
+    reduce (vsum) ((T,Vector), 0, lookup("Numeric","zero").get, "(a,b) => a+b")
     
-    val vprint = op (Vector) ("pprint", infix, List(T), List(Vector), MUnit, foreach((T,Vector), 0, "a => println(a)"), effect = simple) // will print out of order in parallel, but hey
+    // will print out of order in parallel, but hey
+    val vprint = op (Vector) ("pprint", infix, List(T), List(Vector), MUnit, effect = simple) 
+    foreach (vprint) ((T,Vector), 0, "a => println(a)") 
          
-    val vslice = op (Vector) ("slice", infix, List(T), List(Vector, MInt, MInt), Vector, single(Vector, { 
+    val vslice = op (Vector) ("slice", infix, List(T), List(Vector, MInt, MInt), Vector) 
+    single (vslice) (Vector, { 
       // inside single tasks we use normal DSL code just like applications would (modulo arg names)
       stream.printLines(
         "val st = " + quotedArg(1),
@@ -103,32 +121,42 @@ trait SimpleVectorDSL extends ForgeApplication with ScalaOps {
         "  i += 1",
         "}",
         "out"
-      )}))        
+    )})
                      
     // -- getters and setters
   
-    val vrawdata = op (Vector) ("vector_raw_data", compiler, List(T), List(Vector), DArray(T), getter(0, "_data"))
-    val vsetrawdata = op (Vector) ("vector_set_raw_data", compiler, List(T), List(Vector, DArray(T)), MUnit, setter(0, "_data", quotedArg(1)), effect = write(0))
-    val vlength = op (Vector) ("length", infix, List(T), List(Vector), MInt, getter(0, "_length"))    
-    val vsetsize = op (Vector) ("vector_set_size", compiler, List(T), List(Vector,MInt), MUnit, setter(0, "_length", quotedArg(1)), effect = write(0))
+    val vrawdata = op (Vector) ("vector_raw_data", compiler, List(T), List(Vector), DArray(T)) 
+    getter (vrawdata) (0, "_data")
+
+    val vsetrawdata = op (Vector) ("vector_set_raw_data", compiler, List(T), List(Vector, DArray(T)), MUnit, effect = write(0))
+    setter (vsetrawdata) (0, "_data", quotedArg(1))
+
+    val vlength = op (Vector) ("length", infix, List(T), List(Vector), MInt) 
+    getter (vlength) (0, "_length")
+
+    val vsetsize = op (Vector) ("vector_set_size", compiler, List(T), List(Vector,MInt), MUnit) 
+    setter (vsetsize) (0, "_length", quotedArg(1)), effect = write(0)
     
     // -- data manipulation
     
     // TODO: try to make SimpleVector use DeliteArrayBuffer instead of defining its own buffer methods
     // what should the dc methods do then? just call the underlying the _data methods..
         
-    op (Vector) ("insert", infix, List(T), List(Vector,MInt,T), MUnit, single(MUnit, {
+    val vinsert = op (Vector) ("insert", infix, List(T), List(Vector,MInt,T), MUnit, effect = write(0))
+    single (vinsert) (MUnit, {
       stream.printLines(
         "vector_insertspace("+quotedArg(0)+","+quotedArg(1)+",1)",
         quotedArg(0)+"("+quotedArg(1)+") = " + quotedArg(2)
-    )}), effect = write(0))
+    )})
 
-    val vappend = op (Vector) ("append", infix, List(T), List(Vector,MInt,T), MUnit, single(MUnit, {
+    val vappend = op (Vector) ("append", infix, List(T), List(Vector,MInt,T), MUnit, effect = write(0))
+    single (vappend) (MUnit, {
       quotedArg(0)+".insert("+quotedArg(0)+".length, "+quotedArg(2)+")"
-    }), effect = write(0))
+    })
         
     
-    op (Vector) ("vector_insertspace", compiler, List(T), List(Vector,MInt,MInt), MUnit, single(MUnit, {
+    val vinsertspace = op (Vector) ("vector_insertspace", compiler, List(T), List(Vector,MInt,MInt), MUnit, effect = write(0))
+    single (vinsertspace) (MUnit, {
       val v = quotedArg(0)
       val pos = quotedArg(1)
       val len = quotedArg(2)
@@ -138,9 +166,10 @@ trait SimpleVectorDSL extends ForgeApplication with ScalaOps {
         "val data = vector_raw_data("+v+")",
         "darray_copy(data, "+pos+", data, "+pos+" + "+len+", "+v+".length - "+pos+")",
         "vector_set_size("+v+", "+v+".length + "+len+")"
-    )}), effect = write(0))
+    )})
     
-    op (Vector) ("vector_ensureextra", compiler, List(T), List(Vector,MInt), MUnit, single(MUnit, {
+    val vensureextra = op (Vector) ("vector_ensureextra", compiler, List(T), List(Vector,MInt), MUnit, effect = write(0)) 
+    single (vensureextra) (MUnit, {
       val v = quotedArg(0)
       val extra = quotedArg(1)
       stream.printLines(        
@@ -148,9 +177,10 @@ trait SimpleVectorDSL extends ForgeApplication with ScalaOps {
         "if (data.length - "+v+".length < "+extra+") {",
         "  vector_realloc("+v+", "+v+".length + "+extra+")",
         "}"
-    )}), effect = write(0))        
+    )})
     
-    op (Vector) ("vector_realloc", compiler, List(T), List(Vector,MInt), MUnit, single(MUnit, {
+    val vrealloc = op (Vector) ("vector_realloc", compiler, List(T), List(Vector,MInt), MUnit, effect = write(0))
+    single (vrealloc) (MUnit, {
       val v = quotedArg(0)
       val minLen = quotedArg(1)
       stream.printLines(        
@@ -160,9 +190,8 @@ trait SimpleVectorDSL extends ForgeApplication with ScalaOps {
         "val d = darray_new[T](n)",
         "darray_copy(data, 0, d, 0, "+v+".length)",        
         "vector_set_raw_data("+v+", d.unsafeImmutable)"    
-    )}), effect = write(0))        
+    )})
             
-                  
     /**
      * DeliteCollectionification
      * This enables a tpe to be passed in as the collection type of a Delite op
@@ -176,15 +205,17 @@ trait SimpleVectorDSL extends ForgeApplication with ScalaOps {
     //     "if (" + quotedArg(1) + ") " + quote(parBuffer) + " else " + quote(parFlat)
     // }))
         
-    val vappendable = op (Vector) ("vector_appendable", compiler, List(T), List(Vector,MInt,T), MBoolean, single(MBoolean, {
+    val vappendable = op (Vector) ("vector_appendable", compiler, List(T), List(Vector,MInt,T), MBoolean)
+    single (vappendable) (MBoolean, {
       "true" 
-    }))
+    })
 
-    val vcopy = op (Vector) ("vector_copy", compiler, List(T), List(Vector,MInt,Vector,MInt,MInt), MUnit, single(MUnit, {
+    val vcopy = op (Vector) ("vector_copy", compiler, List(T), List(Vector,MInt,Vector,MInt,MInt), MUnit, effect = write(2))
+    single (vcopy) (MUnit, {
       val src = "vector_raw_data(" + quotedArg(0) + ")"
       val dest = "vector_raw_data(" + quotedArg(2) + ")"
       "darray_copy("+src+","+quotedArg(1)+","+dest+","+quotedArg(3)+","+quotedArg(4)+")"
-    }), effect = write(2))
+    })
     
     Vector is DeliteCollectionBuffer(T, vnew, vlength, vapply, vupdate, /*vparallelization,*/ vsetsize, vappendable, vappend, vcopy)
         

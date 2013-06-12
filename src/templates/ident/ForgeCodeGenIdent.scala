cc1121c083e24d43c5ea156f8a07ea83d8ff5d78
Tiark Rompf <tiark.rompf@epfl.ch>
2013-06-12 20:19:06 +0200

    strip stuff from main generator

--- src/templates/ident/ForgeCodeGenIdent.scala
+++ src/templates/ident/ForgeCodeGenIdent.scala
@@ -19,92 +19,114 @@ trait ForgeCodeGenIdent extends ForgeCodeGenBackend with LibGenPackages with Bas
     
   def emitDSLImplementation() {
     Directory(Path(dslDir)).createDirectory()
-    emitDSLDefinition()
-    emitClasses()
+    emitDSLSpecification()
   }
   
-  def emitDSLDefinition() {
-    val dslStream = new PrintWriter(new FileWriter(dslDir+dsl+".scala")) 
+  def emitDSLSpecification() {
+    val dslStream = new PrintWriter(new FileWriter(dslDir+dsl+"DSLI.scala")) 
     dslStream.println("package " + packageName)
     dslStream.println()
-    emitAllImports(dslStream)
+    dslStream.println("import ppl.dsl.forge.core.{ForgeApplication,ForgeApplicationRunner}")
+    dslStream.println()
+    dslStream.println("object " + dsl + "DSLRunner extends ForgeApplicationRunner with " + dsl + "DSL")
+    dslStream.println()
+    dslStream.println("trait " + dsl + "DSL extends ForgeApplication{")
+    dslStream.println("/**")
+    dslStream.println(" * The name of your DSL. This is the name that will be used in generated files,")
+    dslStream.println(" * package declarations, etc.")
+    dslStream.println(" */")
+    dslStream.println("def dslName = \"SimpleVector\"")
+    dslStream.println("")
+    dslStream.println("/**")
+    dslStream.println(" * The specification is the DSL definition (types, data structures, ops)")
+    dslStream.println(" */")
+    dslStream.println("def specification() = {")
+
+    // internals go here
+    emitClasses(dslStream)
+
+    dslStream.println("}")
+    dslStream.println("}")
+    dslStream.close()
+/*  emitAllImports(dslStream)
     dslStream.println()
     emitApplicationRunner(OpsGrp.values.toList, dslStream)
     dslStream.println()    
     emitDSLPackageDefinitions(OpsGrp.values.toList, dslStream)
     dslStream.println()    
-    dslStream.close()
+*/
   }  
     
-  def emitClasses() {
-    val clsDir = dslDir + File.separator + "classes"
-    Directory(Path(clsDir)).createDirectory()
-    
-    // trait to group all of the ops together
-    val grpStream = new PrintWriter(new FileWriter(clsDir+File.separator+dsl+"Classes.scala"))    
-    grpStream.println("package " + packageName + ".classes")
-    grpStream.println()
-    emitScalaReflectImports(grpStream)
-    emitLMSImports(grpStream)
-    emitDSLImports(grpStream)    
-    grpStream.println()
-    grpStream.print("trait " + dsl + "Classes extends ")
+  def emitClasses(grpStream: PrintWriter) {
+    //grpStream.println("package " + packageName + ".classes")
+    //grpStream.println()
+    //emitScalaReflectImports(grpStream)
+    //emitLMSImports(grpStream)
+    //emitDSLImports(grpStream)    
+    //grpStream.println()
+    //grpStream.print("trait " + dsl + "Classes extends ")
     var first = true
     for ((grp,opsGrp) <- OpsGrp) {
+
+      grpStream.println("// "+grp.name + " / " + opsGrp.name)
+
       if (isTpeClass(grp)) {
-        if (first) grpStream.print(opsGrp.name) else grpStream.print(" with " + opsGrp.name)
+        //if (first) grpStream.print(opsGrp.name) else grpStream.print(" with " + opsGrp.name)
         first = false
       }
       else if (!isTpeClass(grp) && !isTpeClassInst(grp)) {        
         val wrapper = grp.name + "Wrapper"
-        if (first) grpStream.print(wrapper) else grpStream.print(" with " + wrapper)
+        //if (first) grpStream.print(wrapper) else grpStream.print(" with " + wrapper)
         first = false
       
-        val stream = new PrintWriter(new FileWriter(clsDir+File.separator+grp.name+".scala"))            
-        stream.println("package " + packageName + ".classes")
-        stream.println()
-        emitScalaReflectImports(stream)
-        emitScalaMathImports(stream)      
-        emitLMSImports(stream)
-        emitDSLImports(stream)      
-        stream.println()
-        stream.println("trait " + wrapper + " {")
+        val stream = grpStream
+        //stream.println("package " + packageName + ".classes")
+        //stream.println()
+        //emitScalaReflectImports(stream)
+        //emitScalaMathImports(stream)      
+        //emitLMSImports(stream)
+        //emitDSLImports(stream)      
+        //stream.println()
+        //stream.println("trait " + wrapper + " {")
         // stream.println("trait " + wrapper + " extends " + grp.name + "Ops with " + dsl + "Base {")
-        stream.println( "  this: " + dsl + "Base with " + dsl + "Classes => ")
-        stream.println()
+        //stream.println( "  this: " + dsl + "Base with " + dsl + "Classes => ")
+        //stream.println()
         emitClass(opsGrp, stream)
-        stream.println("}")
-        stream.println()
-        stream.close()
+        //stream.println("}")
+        //stream.println()
+        //stream.close()
       
         // because front-end types are not in scope (to prevent unintentional recursive calls and ambiguities),
         // we need to factor almost all tasks out to a separate trait in the library version also
         if (opsGrp.ops.exists(requiresImpl)) {
-          val implStream = new PrintWriter(new FileWriter(clsDir+File.separator+grp.name+"WrapperImpl"+".scala"))
-          implStream.println("package " + packageName + ".classes")       
-          implStream.println()
-          emitScalaReflectImports(implStream) 
-          emitScalaMathImports(implStream)
-          emitDSLImports(implStream)
-          implStream.println()
+          val implStream = grpStream
+          //implStream.println("package " + packageName + ".classes")       
+          //implStream.println()
+          //emitScalaReflectImports(implStream) 
+          //emitScalaMathImports(implStream)
+          //emitDSLImports(implStream)
+          //implStream.println()
           emitImpls(opsGrp, implStream)
-          implStream.close()      
+          //implStream.close()      
         
-          grpStream.print(" with " + grp.name + "WrapperImpl")              
+          //grpStream.print(" with " + grp.name + "WrapperImpl")
         }
       }
-    }       
+    }
+
+
     for (d <- DataStructs.keys.toSeq diff OpsGrp.keys.filter(grpIsTpe).map(grpAsTpe).toSeq) {
       warn("(ident) ignoring data definition for " + d.name + " since it cannot be instantiated in app code (it has no accompanying ops)")
-    }             
-    for (e <- Externs) {
+    }
+
+    /*for (e <- Externs) {
       grpStream.print(" with " + e.opsGrp.grp.name + "Wrapper")
     }    
     grpStream.println("{")
     grpStream.println("  this: " + dsl + "Lib with " + dsl + "Application => ")
     grpStream.println("}")
-    grpStream.println()
-    grpStream.close()
+    grpStream.println()*/
+    //grpStream.close()
   }    
 }
 

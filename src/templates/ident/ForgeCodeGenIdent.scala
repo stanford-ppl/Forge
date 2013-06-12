510eef605e2ef55f1f579d486c5153a4b0bc82ff
Tiark Rompf <tiark.rompf@epfl.ch>
2013-06-12 06:02:35 +0200

    copy library gen as basis for identity gen

---
+++ src/templates/ident/ForgeCodeGenIdent.scala
@@ -0,0 +1,110 @@
+package ppl.dsl.forge
+package templates
+package ident
+
+import java.io.{File,PrintWriter,FileWriter}
+import scala.tools.nsc.io.{Directory,Path}
+import scala.reflect.SourceContext
+import scala.virtualization.lms.common._
+import scala.virtualization.lms.internal.{GenericFatCodegen, GenericCodegen}
+
+import core._
+import shared._
+
+trait ForgeCodeGenIdent extends ForgeCodeGenBackend with LibGenPackages with BaseGenOps with LibGenImports with LibGenOps {  
+  val IR: ForgeApplicationRunner with ForgeExp  
+  import IR._
+  
+  lazy val packageName = dsl.toLowerCase() + ".ident"
+    
+  def emitDSLImplementation() {
+    Directory(Path(dslDir)).createDirectory()
+    emitDSLDefinition()
+    emitClasses()
+  }
+  
+  def emitDSLDefinition() {
+    val dslStream = new PrintWriter(new FileWriter(dslDir+dsl+".scala")) 
+    dslStream.println("package " + packageName)
+    dslStream.println()
+    emitAllImports(dslStream)
+    dslStream.println()
+    emitApplicationRunner(OpsGrp.values.toList, dslStream)
+    dslStream.println()    
+    emitDSLPackageDefinitions(OpsGrp.values.toList, dslStream)
+    dslStream.println()    
+    dslStream.close()
+  }  
+    
+  def emitClasses() {
+    val clsDir = dslDir + File.separator + "classes"
+    Directory(Path(clsDir)).createDirectory()
+    
+    // trait to group all of the ops together
+    val grpStream = new PrintWriter(new FileWriter(clsDir+File.separator+dsl+"Classes.scala"))    
+    grpStream.println("package " + packageName + ".classes")
+    grpStream.println()
+    emitScalaReflectImports(grpStream)
+    emitLMSImports(grpStream)
+    emitDSLImports(grpStream)    
+    grpStream.println()
+    grpStream.print("trait " + dsl + "Classes extends ")
+    var first = true
+    for ((grp,opsGrp) <- OpsGrp) {
+      if (isTpeClass(grp)) {
+        if (first) grpStream.print(opsGrp.name) else grpStream.print(" with " + opsGrp.name)
+        first = false
+      }
+      else if (!isTpeClass(grp) && !isTpeClassInst(grp)) {        
+        val wrapper = grp.name + "Wrapper"
+        if (first) grpStream.print(wrapper) else grpStream.print(" with " + wrapper)
+        first = false
+      
+        val stream = new PrintWriter(new FileWriter(clsDir+File.separator+grp.name+".scala"))            
+        stream.println("package " + packageName + ".classes")
+        stream.println()
+        emitScalaReflectImports(stream)
+        emitScalaMathImports(stream)      
+        emitLMSImports(stream)
+        emitDSLImports(stream)      
+        stream.println()
+        stream.println("trait " + wrapper + " {")
+        // stream.println("trait " + wrapper + " extends " + grp.name + "Ops with " + dsl + "Base {")
+        stream.println( "  this: " + dsl + "Base with " + dsl + "Classes => ")
+        stream.println()
+        emitClass(opsGrp, stream)
+        stream.println("}")
+        stream.println()
+        stream.close()
+      
+        // because front-end types are not in scope (to prevent unintentional recursive calls and ambiguities),
+        // we need to factor almost all tasks out to a separate trait in the library version also
+        if (opsGrp.ops.exists(requiresImpl)) {
+          val implStream = new PrintWriter(new FileWriter(clsDir+File.separator+grp.name+"WrapperImpl"+".scala"))
+          implStream.println("package " + packageName + ".classes")       
+          implStream.println()
+          emitScalaReflectImports(implStream) 
+          emitScalaMathImports(implStream)
+          emitDSLImports(implStream)
+          implStream.println()
+          emitImpls(opsGrp, implStream)
+          implStream.close()      
+        
+          grpStream.print(" with " + grp.name + "WrapperImpl")              
+        }
+      }
+    }       
+    for (d <- DataStructs.keys.toSeq diff OpsGrp.keys.filter(grpIsTpe).map(grpAsTpe).toSeq) {
+      warn("(ident) ignoring data definition for " + d.name + " since it cannot be instantiated in app code (it has no accompanying ops)")
+    }             
+    for (e <- Externs) {
+      grpStream.print(" with " + e.opsGrp.grp.name + "Wrapper")
+    }    
+    grpStream.println("{")
+    grpStream.println("  this: " + dsl + "Lib with " + dsl + "Application => ")
+    grpStream.println("}")
+    grpStream.println()
+    grpStream.close()
+  }    
+}
+
---
+++ src/templates/ident/Imports.scala
@@ -0,0 +1,31 @@
+package ppl.dsl.forge
+package templates
+package ident
+
+import java.io.PrintWriter
+import core._
+import shared.BaseGenImports
+
+trait LibGenImports extends BaseGenImports {  
+  this: ForgeCodeGenIdent =>
+  
+  val IR: ForgeApplicationRunner with ForgeExp 
+  import IR._
+      
+  def emitScalaMathImports(stream: PrintWriter) {
+    stream.println("import scala.math.Ordering.Implicits._")
+    stream.println("import scala.math.Numeric.Implicits._")
+  } 
+     
+  override def emitScalaImports(stream: PrintWriter) {
+    super.emitScalaImports(stream)
+    emitScalaMathImports(stream)
+  }
+        
+  override def emitDSLImports(stream: PrintWriter) {
+    super.emitDSLImports(stream)
+    stream.println("import " + packageName + "._")
+    stream.println("import " + packageName + ".classes._")
+    // stream.println("import " + dsl.toLowerCase() + ".library.extern._")    
+  }   
+}

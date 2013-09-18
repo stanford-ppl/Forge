1376964c48f41cf161a75032b20f8e4f1e35fcee
Chris De Sa <cdesa@stanford.edu>
2013-09-18 15:59:16 -0700

    Convergence conditions coded, now working on nonoptimized step.

--- apps/OptiML/src/PDIPSolver.scala
+++ apps/OptiML/src/PDIPSolver.scala
@@ -13,6 +13,14 @@ trait PDIPSolver extends OptiMLApplication {
 
   def main() = {
 
+    val MAXITERS = 100;
+    val ABSTOL = 1e-8;
+    val RELTOL = 1e-8;
+    val FEASTOL = 1e-8;
+    val MINSLACK = 1e-8;      
+    val STEP = 0.99;
+    val EXPON = 3;
+
     if (args.length != 9) {
       print_usage
     }
@@ -65,21 +73,85 @@ trait PDIPSolver extends OptiMLApplication {
     val tau0 = 1.0
     val lambda0 = 1.0
     val theta0 = 1.0
-    val u0 = DenseVector(tau0) << z0
-    val v0 = y0 << x0 << DenseVector(theta0)
-    val w0 = DenseVector(lambda0) << s0
+    val u0 = DenseVector(tau0).t << z0
+    val v0 = y0 << x0 << DenseVector(theta0).t
+    val w0 = DenseVector(lambda0).t << s0
 
     val dimu = m+1
     val dimv = p+n+1
     val dimw = m+1
 
-    implicit def diffPDIP(t1: Rep[Tup7[DenseVector[Double],DenseVector[Double],DenseVector[Double],DenseVector[Double],DenseVector[Double],DenseVector[Double],DenseVector[Double]]],
-                         t2: Rep[Tup7[DenseVector[Double],DenseVector[Double],DenseVector[Double],DenseVector[Double],DenseVector[Double],DenseVector[Double],DenseVector[Double]]]) = unit(10.0)
+    implicit def diffPDIP(t1: Rep[Tup4[DenseVector[Double],DenseVector[Double],DenseVector[Double],DenseVector[Double]]],
+                          t2: Rep[Tup4[DenseVector[Double],DenseVector[Double],DenseVector[Double],DenseVector[Double]]]) = {
+      val (u, v, w, viters) = t4(t2)
+      val iters = viters(0)
 
-    val result = untilconverged((x0,s0,y0,z0,u0,v0,w0), maxIter = 100) { cur =>
-      val (x, s, y, z, u, v, w) = t7(cur)
-      println(norm(x))
-      (x, s, y, z, u, v, w)
+      val tau = u(0)
+      val z = u.slice(1, m+1)
+      val y = v.slice(0, p)
+      val x = v.slice(p, p+n)
+      val theta = v(p+n)
+      val lambda = w(0)
+      val s = w.slice(1, m+1)
+
+      val pcost = (c*:*x) / tau  
+      val dcost = -(h*:*z + b*:*y)/ tau  
+      val absgap = (u*:*w) / (tau*tau)
+      val relgap = if (dcost > 0.0) {
+        absgap / dcost
+      } 
+      else if (pcost < 0.0) {
+        absgap / (-pcost)
+      }
+      else {
+        Double.PositiveInfinity
+      }
+      val hresi = G*x+s
+      val resi = h - hresi/tau
+      val hrese = A*x
+      val rese = b-hrese/tau
+      val hresd = G.t*z + A.t*y
+      val resd = c+hresd/tau
+      val pres = max(norm(resi)/nrmh, norm(rese)/nrmb)
+      val dres = norm(resd)/nrmc
+      val hpres = max(norm(hresi), norm(hrese))
+      val hdres = norm(hresd)
+      
+      println("%02.0f:% 7.0e% 8.0e% 8.0e% 8.0e ".format(iters - 1.0, absgap, relgap, pres, dres))
+
+      if ((pres <= FEASTOL)&&(dres <= FEASTOL)&&((absgap <= ABSTOL)||(relgap <= RELTOL))) {
+        // optimal
+        0.0
+      }
+      else if ((h*:*z+b*:*y < 0.0)&&(hdres/abs(h*:*z+b*:*y)<= FEASTOL)) {
+        // primal infeasible
+        0.0
+      }
+      else if ((c*:*x < 0.0)&&(hpres/abs(c*:*x) <= FEASTOL)) {
+        // dual infeasible
+        0.0
+      }
+      else {
+        // continue
+        1.0
+      }
+    }
+
+    val result = untilconverged((u0,v0,w0,DenseVector(0.0)), maxIter = MAXITERS) { cur =>
+      val (u, v, w, viters) = t4(cur)
+      val iters = viters(0)
+      
+      val tau = u(0)
+      val z = u.slice(1, m+1)
+      val y = v.slice(0, p)
+      val x = v.slice(p, p+n)
+      val theta = v(p+n)
+      val lambda = w(0)
+      val s = w.slice(1, m+1)
+
+      
+
+      (u, v, w, DenseVector(iters + 1.0))
     }
   }
 

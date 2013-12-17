2b4ec9155ceeffef7a59a74830ab581e93b558c9
Christopher Aberger <christopheraberger@caberger.local>
2013-12-16 21:05:55 -0800

    Renamed test case to BC.

--- src/dsls/optila/DenseVector.scala
+++ src/dsls/optila/DenseVector.scala
@@ -12,7 +12,7 @@ trait DenseVectorOps {
     val R = tpePar("R")
     val B = tpePar("B")
 
-    val DenseVector = lookupTpe("DenseVector")
+    val DenseVector = lookupTpe("DenseVector") // tpe("DenseVector", T)
     val IndexVector = lookupTpe("IndexVector")
     val DenseVectorView = lookupTpe("DenseVectorView")
     val DenseMatrix = lookupTpe("DenseMatrix")

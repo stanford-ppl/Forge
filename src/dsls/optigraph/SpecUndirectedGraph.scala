c51dd1f2d0dccea40aeb4ff0075b165cf6b9b8dd
Christopher Aberger <christopheraberger@dn0a23129b.sunet>
2014-02-25 14:10:45 -0800

    Updated intersection to fix bug.

--- src/dsls/optigraph/SpecUndirectedGraph.scala
+++ src/dsls/optigraph/SpecUndirectedGraph.scala
@@ -70,7 +70,7 @@ trait SpecUndirectedGraphOps{
           var t = 0
           while(i < nbrs.length){
             if(nbrs(i)==nbrsOfNbrs(j)) t += 1
-            while(nbrsOfNbrs(j) < nbrs(i)){
+            while(nbrsOfNbrs(j) < nbrs(i) && j < nbrsOfNbrs.length){
               j += 1
             }
             i += 1

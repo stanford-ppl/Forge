1b5e254d2e325341dbaa053b11b47824aea37ab7
Christopher Aberger <christopheraberger@dnab40465d.stanford.edu>
2014-08-04 16:20:43 -0700

    Added the node to community array in, pretty sure something broke though.

--- apps/OptiGraph/src/CommunityDetection.scala
+++ apps/OptiGraph/src/CommunityDetection.scala
@@ -14,7 +14,7 @@ trait CommDetection extends OptiGraphApplication {
   def main() = {
     println("CommunityDetection")
   
-    if (args.length < 3) printUsage
+    if (args.length < 2) printUsage
 
     tic("input loading")
     val edgeList = loadUndirectedEdgeList(args(0))
@@ -24,19 +24,26 @@ trait CommDetection extends OptiGraphApplication {
     val g = undirectedGraphFromEdgeList(edgeList)
     toc("creating graph",g)
     
-    val k = args(2).toDouble
-    val result = CommunityDetection(args(1),g.getCSRNodes,g.getCSREdges,k)
+    val k = args(1).toDouble
+    val result = CommunityDetection(g.getCSRNodes,g.getCSREdges,k)
 
-    println(array_length(result._1))
+    val newNumNodes = array_length(result._1)
+    println(newNumNodes)
+    var i = 0
+    while(i < array_length(result._3)){
+      if(array_apply(result._3,i) < 0 || array_apply(result._3,i) >= newNumNodes)
+        println("ERROR i: " + i + " data: " + array_apply(result._3,i))
+      i += 1
+    }
   }
-  def CommunityDetection(path: Rep[String], nodes:Rep[ForgeArray[Int]], edges:Rep[ForgeArray[Int]], k:Rep[Double]) : Rep[Tup2[ForgeArray[Int],ForgeArray[Int]]] = {
+  def CommunityDetection(nodes:Rep[ForgeArray[Int]], edges:Rep[ForgeArray[Int]], k:Rep[Double]) : Rep[Tup3[ForgeArray[Int],ForgeArray[Int],ForgeArray[Int]]] = {
     var g = undirectedGraphFromCSR(nodes,edges)
 
     println("performing Community Detection")
     tic(g)
     
     val precision = k
-    var c = Community(g,precision)
+    var c = Community(g,precision,array_fromfunction[Int](g.numNodes,e=>e))
     var mod,newMod = c.modularity
 
     println("Modularity: " + mod)
@@ -54,24 +61,24 @@ trait CommDetection extends OptiGraphApplication {
       ///////////////////PHASE 2/////////////////////////////////////
       //Generate a new graph from community structure
       //Generate a new comm structure from graph so we can go again
-      g = c.generateNewGraph
-      c = Community(g,precision)  //to be used in the next round
+      val (g,n2o) = unpack(c.generateNewGraph)
+      c = Community(g,precision,n2o)  //to be used in the next round
       ///////////////////////////////////////////////////////////////
 
       //////////////////////DEBUG/////////////////////////////////////
       println("Level: " + level + " Modularity improved from: " + mod + " to: " + newMod)
-      println("\tnumNodes: " + g.numNodes + " numEdges: " + g.numEdges + " weight: " + g.totalWeight)
-      printGraph(path,level,mod,newMod,g.numNodes,g.numEdges,g.getCSRNodes,g.getCSREdges,g.getCSREdgeWeights)
+      //println("\tnumNodes: " + g.numNodes + " numEdges: " + g.numEdges + " weight: " + g.totalWeight)
+      //printGraph(path,level,mod,newMod,g.numNodes,g.numEdges,g.getCSRNodes,g.getCSREdges,g.getCSREdgeWeights)
       level += 1
       mod = newMod
       ////////////////////////////////////////////////////////////////
     }
     toc(mod)
 
-    pack(g.getCSRNodes,g.getCSREdges)
+    pack(g.getCSRNodes,g.getCSREdges,c.n2o)
   }
   def printUsage = {
-    println("Usage: CommunityDetection <path to input edge list file> <path prefix for output> <convergance factor>")
+    println("Usage: CommunityDetection <path to input edge list file> <convergance factor>")
     exit(-1)
   }
 }

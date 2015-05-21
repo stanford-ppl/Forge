6ef58f2e6e29f96db33f67ba99b9547d87cea830
Christopher De Sa <cdesa@stanford.edu>
2015-05-20 23:50:20 -0700

    removed functionality to work around bug

--- src/dsls/optiml/Feature.scala
+++ src/dsls/optiml/Feature.scala
@@ -8,76 +8,76 @@ trait FeatureOps {
   this: OptiMLDSL =>
 
   def importFeatureOps() {
-    val DenseVector = lookupTpe("DenseVector")
+    // val DenseVector = lookupTpe("DenseVector")
     
-    val ContinuousFeature = tpe("ContinuousFeature")
-    data(ContinuousFeature, ("_default", MDouble), ("_min", MDouble), ("_max", MDouble))
+    // val ContinuousFeature = tpe("ContinuousFeature")
+    // data(ContinuousFeature, ("_default", MDouble), ("_min", MDouble), ("_max", MDouble))
 
-    // we use a magic number for the default because we don't support optional values in generated DSL code (yet)
-    static (ContinuousFeature) ("apply", Nil, (("default", MDouble, "unit(0.0209)"), ("min", MDouble, "math_ninf()"), ("max", MDouble, "math_inf()")) :: ContinuousFeature) implements allocates(ContinuousFeature, ${$0}, ${$1}, ${$2})
+    // // we use a magic number for the default because we don't support optional values in generated DSL code (yet)
+    // static (ContinuousFeature) ("apply", Nil, (("default", MDouble, "unit(0.0209)"), ("min", MDouble, "math_ninf()"), ("max", MDouble, "math_inf()")) :: ContinuousFeature) implements allocates(ContinuousFeature, ${$0}, ${$1}, ${$2})
 
-    val ContinuousFeatureOps = withTpe(ContinuousFeature)
-    ContinuousFeatureOps {
-      infix ("default") (Nil :: MDouble) implements getter(0, "_default")
-      infix ("min") (Nil :: MDouble) implements getter(0, "_min")
-      infix ("max") (Nil :: MDouble) implements getter(0, "_max")
-      infix ("apply") (MString :: MDouble) implements composite ${ 
-        if ($1 == "" && $self.default != 0.0209) {
-          $self.default
-        }
-        else {
-          val num = $1.toDouble
-          if (num < $self.min) { $self.min }
-          else if (num > $self.max) { $self.max }
-          else num
-        }
-      }
-    }
+    // val ContinuousFeatureOps = withTpe(ContinuousFeature)
+    // ContinuousFeatureOps {
+    //   infix ("default") (Nil :: MDouble) implements getter(0, "_default")
+    //   infix ("min") (Nil :: MDouble) implements getter(0, "_min")
+    //   infix ("max") (Nil :: MDouble) implements getter(0, "_max")
+    //   infix ("apply") (MString :: MDouble) implements composite ${ 
+    //     if ($1 == "" && $self.default != 0.0209) {
+    //       $self.default
+    //     }
+    //     else {
+    //       val num = $1.toDouble
+    //       if (num < $self.min) { $self.min }
+    //       else if (num > $self.max) { $self.max }
+    //       else num
+    //     }
+    //   }
+    // }
 
-    val DiscreteFeature = tpe("DiscreteFeature")
-    data(DiscreteFeature, ("_features", MHashMap(MString, MInt)))
-    compiler (DiscreteFeature) ("discrete_feature_alloc", Nil, (MHashMap(MString, MInt) :: DiscreteFeature)) implements allocates(DiscreteFeature, ${$0})
+    // val DiscreteFeature = tpe("DiscreteFeature")
+    // data(DiscreteFeature, ("_features", MHashMap(MString, MInt)))
+    // compiler (DiscreteFeature) ("discrete_feature_alloc", Nil, (MHashMap(MString, MInt) :: DiscreteFeature)) implements allocates(DiscreteFeature, ${$0})
 
-    // FIXME: this produces an illegal ordering of effect error when 'composite' instead of 'single'. related to combo of array_fromseq (mutable) and FHashMap?    
-    static (DiscreteFeature) ("apply", Nil, (varArgs(MString) :: DiscreteFeature)) implements single ${
-      val keys = array_fromseq($0)
-      val values = array_fromfunction(array_length(keys), i => i)
-      discrete_feature_alloc(fhashmap_from_arrays(keys, values))
-    }
+    // // FIXME: this produces an illegal ordering of effect error when 'composite' instead of 'single'. related to combo of array_fromseq (mutable) and FHashMap?    
+    // static (DiscreteFeature) ("apply", Nil, (varArgs(MString) :: DiscreteFeature)) implements single ${
+    //   val keys = array_fromseq($0)
+    //   val values = array_fromfunction(array_length(keys), i => i)
+    //   discrete_feature_alloc(fhashmap_from_arrays(keys, values))
+    // }
 
-    val DiscreteFeatureOps = withTpe(DiscreteFeature)
-    DiscreteFeatureOps {
-      compiler ("getFeatures") (Nil :: MHashMap(MString, MInt)) implements getter(0, "_features")
-      infix ("apply") (MString :: MInt) implements composite ${ 
-        val featureMap = getFeatures($self)
-        if (featureMap.contains($1)) featureMap($1) else 0
-      }
+    // val DiscreteFeatureOps = withTpe(DiscreteFeature)
+    // DiscreteFeatureOps {
+    //   compiler ("getFeatures") (Nil :: MHashMap(MString, MInt)) implements getter(0, "_features")
+    //   infix ("apply") (MString :: MInt) implements composite ${ 
+    //     val featureMap = getFeatures($self)
+    //     if (featureMap.contains($1)) featureMap($1) else 0
+    //   }
 
-      // convert this feature value into an indicator or dummy variable encoding
-      infix ("indicator") (MString :: DenseVector(MInt)) implements composite ${
-        val featureMap = getFeatures($self)
-        val numKeys = fhashmap_size(featureMap)
-        val e = if (featureMap.contains($1)) featureMap($1) else 0
-        (0::numKeys) { i => if (i == e) 1 else 0 }
-      }
+    //   // convert this feature value into an indicator or dummy variable encoding
+    //   infix ("indicator") (MString :: DenseVector(MInt)) implements composite ${
+    //     val featureMap = getFeatures($self)
+    //     val numKeys = fhashmap_size(featureMap)
+    //     val e = if (featureMap.contains($1)) featureMap($1) else 0
+    //     (0::numKeys) { i => if (i == e) 1 else 0 }
+    //   }
 
-    }
+    // }
 
-    val BinaryFeature = tpe("BinaryFeature")
-    data(BinaryFeature, ("_default", MBoolean))
-    static (BinaryFeature) ("apply", Nil, ("default", MBoolean, "unit(false)") :: BinaryFeature) implements allocates(BinaryFeature, ${$0})
+    // val BinaryFeature = tpe("BinaryFeature")
+    // data(BinaryFeature, ("_default", MBoolean))
+    // static (BinaryFeature) ("apply", Nil, ("default", MBoolean, "unit(false)") :: BinaryFeature) implements allocates(BinaryFeature, ${$0})
 
-    val BinaryFeatureOps = withTpe(BinaryFeature)
-    BinaryFeatureOps {
-      infix ("default") (Nil :: MBoolean) implements getter(0, "_default")
+    // val BinaryFeatureOps = withTpe(BinaryFeature)
+    // BinaryFeatureOps {
+    //   infix ("default") (Nil :: MBoolean) implements getter(0, "_default")
 
-      infix ("apply") (MString :: MInt) implements composite ${ 
-        fassert($1 == "" || $1 == "0" || $1 == "1" || $1 == "false" || $1 == "true", "illegal input to binary feature")
-        if ($1 == "0" || $1 == "false") 0
-        else if ($1 == "1" || $1 == "true") 1
-        else if ($self.default) 1
-        else 0        
-      }
-    }
+    //   infix ("apply") (MString :: MInt) implements composite ${ 
+    //     fassert($1 == "" || $1 == "0" || $1 == "1" || $1 == "false" || $1 == "true", "illegal input to binary feature")
+    //     if ($1 == "0" || $1 == "false") 0
+    //     else if ($1 == "1" || $1 == "true") 1
+    //     else if ($self.default) 1
+    //     else 0        
+    //   }
+    // }
   }
 }
--- src/dsls/optiml/Sets.scala
+++ src/dsls/optiml/Sets.scala
@@ -8,42 +8,42 @@ trait SetOps {
   this: OptiMLDSL =>
 
   def importSetOps() {
-    val DenseMatrix = lookupTpe("DenseMatrix")
-    val DenseVector = lookupTpe("DenseVector")
-    val DenseVectorView = lookupTpe("DenseVectorView")
-    val IndexVector = lookupTpe("IndexVector")
+    // val DenseMatrix = lookupTpe("DenseMatrix")
+    // val DenseVector = lookupTpe("DenseVector")
+    // val DenseVectorView = lookupTpe("DenseVectorView")
+    // val IndexVector = lookupTpe("IndexVector")
 
-    val D = tpePar("D")
-    val L = tpePar("L")
+    // val D = tpePar("D")
+    // val L = tpePar("L")
 
-    val TrainingSet = tpe("TrainingSet", (D,L))
-    data(TrainingSet, ("_data", DenseMatrix(D)), ("_labels", DenseVector(L)))
-    static (TrainingSet) ("apply", (D,L), (DenseMatrix(D), DenseVector(L)) :: TrainingSet(D,L)) implements allocates(TrainingSet, ${$0}, ${$1})
+    // val TrainingSet = tpe("TrainingSet", (D,L))
+    // data(TrainingSet, ("_data", DenseMatrix(D)), ("_labels", DenseVector(L)))
+    // static (TrainingSet) ("apply", (D,L), (DenseMatrix(D), DenseVector(L)) :: TrainingSet(D,L)) implements allocates(TrainingSet, ${$0}, ${$1})
 
-    val TrainingSetOps = withTpe(TrainingSet)
-    TrainingSetOps {
-      infix ("labels") (Nil :: DenseVector(L)) implements getter(0, "_labels")
-    }
+    // val TrainingSetOps = withTpe(TrainingSet)
+    // TrainingSetOps {
+    //   infix ("labels") (Nil :: DenseVector(L)) implements getter(0, "_labels")
+    // }
 
-    // is there any use for a separate TestSet type (readability)? to actually measure error, 
-    // TestSets will typically have labels, and therefore be structurally identical to TrainingSet
+    // // is there any use for a separate TestSet type (readability)? to actually measure error, 
+    // // TestSets will typically have labels, and therefore be structurally identical to TrainingSet
 
-    // val TestSet = tpe("TestSet", D)
-    // data(TestSet, ("_data", DenseMatrix(D)))
-    // static (TestSet) ("apply", D, DenseMatrix(D) :: TestSet(D)) implements allocates(TestSet, ${$0})
+    // // val TestSet = tpe("TestSet", D)
+    // // data(TestSet, ("_data", DenseMatrix(D)))
+    // // static (TestSet) ("apply", D, DenseMatrix(D) :: TestSet(D)) implements allocates(TestSet, ${$0})
 
-    for (t <- List(TrainingSet/*, TestSet*/)) {
-      val ops = withTpe(t)
-      ops {
-        infix ("data") (Nil :: DenseMatrix(D)) implements getter(0, "_data")
+    // for (t <- List(TrainingSet/*, TestSet*/)) {
+    //   val ops = withTpe(t)
+    //   ops {
+    //     infix ("data") (Nil :: DenseMatrix(D)) implements getter(0, "_data")
 
-        infix ("apply") ((MInt,MInt) :: D) implements composite ${ $self.data.apply($1,$2) }
-        infix ("apply") (MInt :: DenseVectorView(D)) implements composite ${ $self.data.apply($1) }
-        // infix ("apply") (IndexVector :: t) implements composite ${ TrainingSet($self.data.apply($1),$self.labels.apply($1)) }   // scalac typer crash...
+    //     infix ("apply") ((MInt,MInt) :: D) implements composite ${ $self.data.apply($1,$2) }
+    //     infix ("apply") (MInt :: DenseVectorView(D)) implements composite ${ $self.data.apply($1) }
+    //     // infix ("apply") (IndexVector :: t) implements composite ${ TrainingSet($self.data.apply($1),$self.labels.apply($1)) }   // scalac typer crash...
 
-        infix ("numSamples") (Nil :: MInt) implements composite ${ $self.data.numRows }
-        infix ("numFeatures") (Nil :: MInt) implements composite ${ $self.data.numCols }
-      }
-    }
+    //     infix ("numSamples") (Nil :: MInt) implements composite ${ $self.data.numRows }
+    //     infix ("numFeatures") (Nil :: MInt) implements composite ${ $self.data.numCols }
+    //   }
+    // }
   }
 }

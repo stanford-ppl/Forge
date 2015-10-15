8987fcc3d22a0e6f15ba01682be881b2af14625b
Christopher De Sa <cdesa@stanford.edu>
2015-10-15 04:24:59 -0700

    sync

--- apps/OptiML/src/LPTest.scala
+++ apps/OptiML/src/LPTest.scala
@@ -2,12 +2,15 @@ import optiml.compiler._
 import optiml.library._
 import optiml.shared._
 
-object LPTestCompiler extends OptiMLApplicationCompiler with LPTest
-object LPTestInterpreter extends OptiMLApplicationInterpreter with LPTest
+object LPTest8Compiler extends OptiMLApplicationCompiler with LPTest8
+object LPTest8Interpreter extends OptiMLApplicationInterpreter with LPTest8
 
-trait LPTest extends OptiMLApplication {
+object LPTest16Compiler extends OptiMLApplicationCompiler with LPTest16
+object LPTest16Interpreter extends OptiMLApplicationInterpreter with LPTest16
+
+trait LPTest8 extends OptiMLApplication {
   def printUsage = {
-    println("Usage: LPTest <test size>")
+    println("Usage: LPTest8 <test size>")
     exit(-1)
   }
 
@@ -16,76 +19,66 @@ trait LPTest extends OptiMLApplication {
 
     val test_size = args(0).toInt
 
-    val x8 = (0::test_size) { i =>
+    val x = (0::test_size) { i =>
         ((i + 234) * (i + 344534345)).toByte
     }
 
-    val y8 = (0::test_size) { i =>
+    val y = (0::test_size) { i =>
         ((i + 232) * (i + 345654465)).toByte
     }
 
-    tic("dot", x8, y8)
-    //val xydot = (x8 *:* y8)
-    val xydot = sum(0, test_size) { i => x8(i) * y8(i)}
+    tic("dot", x, y)
+    //val xydot = (x *:* y)
+    val xydot = sum(0, test_size) { i => x(i) * y(i)}
     toc("dot", xydot)
-    println("dot8(x8, y8): " + xydot)
+    println("dot(x, y): " + xydot)
 
-    tic("fpdot8", x8, y8)
-    val xyfpdot = sum(0, test_size) { i => (x8(i).toFloat) * (y8(i).toFloat)}
-    toc("fpdot8", xyfpdot)
-    println("fpdot8(x8, y8): " + xyfpdot)
+    tic("fpdot", x, y)
+    val xyfpdot = sum(0, test_size) { i => (x(i).toFloat) * (y(i).toFloat)}
+    toc("fpdot", xyfpdot)
+    println("fpdot8(x, y): " + xyfpdot)
 
-    tic("lpdot", x8, y8)
-    val xylpdot = DenseVector.lpdot(x8, y8)
-    toc("lpdot8", xylpdot)
-    println("lpdot8(x8, y8): " + xylpdot)
+    tic("lpdot", x, y)
+    val xylpdot = DenseVector.lpdot(x, y)
+    toc("lpdot", xylpdot)
+    println("lpdot8(x, y): " + xylpdot)
+  }
+}
 
 
+trait LPTest16 extends OptiMLApplication {
+  def printUsage = {
+    println("Usage: LPTest16 <test size>")
+    exit(-1)
+  }
 
-    val x16 = (0::test_size) { i =>
+  def main() = {
+    if (args.length < 1) printUsage
+
+    val test_size = args(0).toInt
+
+    val x = (0::test_size) { i =>
         ((i + 234) * (i + 344534345)).toShort
     }
 
-    val y16 = (0::test_size) { i =>
+    val y = (0::test_size) { i =>
         ((i + 232) * (i + 345654465)).toShort
     }
 
-    tic("dot16", x16, y16)
-    //val xydot = (x16 *:* y16)
-    val xydot16 = sum(0, test_size) { i => x16(i) * y16(i)}
-    toc("dot16", xydot16)
-    println("dot(x16, y16): " + xydot16)
+    tic("dot", x, y)
+    //val xydot = (x *:* y)
+    val xydot = sum(0, test_size) { i => x(i) * y(i)}
+    toc("dot", xydot)
+    println("dot(x, y): " + xydot)
 
-    tic("fpdot16", x16, y16)
-    val xyfpdot16 = sum(0, test_size) { i => (x16(i).toFloat) * (y16(i).toFloat)}
-    toc("fpdot16", xyfpdot16)
-    println("fpdot(x16, y16): " + xyfpdot16)
+    tic("fpdot", x, y)
+    val xyfpdot = sum(0, test_size) { i => (x(i).toFloat) * (y(i).toFloat)}
+    toc("fpdot", xyfpdot)
+    println("fpdot8(x, y): " + xyfpdot)
 
-    tic("lpdot16", x16, y16)
-    val xylpdot16 = DenseVector.lpdot(x16, y16)
-    toc("lpdot16", xylpdot16)
-    println("lpdot(x16, y16): " + xylpdot16)
-
-    // val x8 = args(0).toByte
-    // val y8 = args(1).toByte
-
-    // println("x8: " + x8)
-    // println("y8: " + y8)
-    // println("x8 + y8: " + (x8 + y8))
-    // println("x8 * y8: " + (x8 * y8))
-
-    // val x16 = args(0).toShort
-    // val y16 = args(1).toShort
-
-    // println("x16: " + x16)
-    // println("y16: " + y16)
-    // println("x16 + y16: " + (x16 + y16))
-    // println("x16 * y16: " + (x16 * y16))
-
-    // val v8 = DenseVector(x8, y8)
-    // val n2: Rep[Byte] = (v8 *:* v8)
-    
-    // println("dot(v8, v8): " + n2)
-    // println("lpdot(v8, v8): " + DenseVector.lpdot(v8, v8))
+    tic("lpdot", x, y)
+    val xylpdot = DenseVector.lpdot(x, y)
+    toc("lpdot", xylpdot)
+    println("lpdot8(x, y): " + xylpdot)
   }
 }

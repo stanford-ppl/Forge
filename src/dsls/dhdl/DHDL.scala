44a4880f7e2a961edd0e1e190d22cab2f4fdf75d
Yaqi Zhang <hsbzyq@gmail.com>
2016-01-09 22:58:53 -0800

    add type and type conversion

--- src/dsls/dhdl/DHDL.scala
+++ src/dsls/dhdl/DHDL.scala
@@ -25,6 +25,19 @@ trait DHDLDSL extends ForgeApplication
 		lift(Prim) (MFixPt)
 		lift(Prim) (MBoolean)
 
+		val TConv = grp("TypeConv") 
+		val float2FixPt = infix (TConv) ("toFixPt", Nil, MFloat::MFixPt)	
+		impl (float2FixPt) (codegen($cala, ${$0.toInt})) 
+		val fixPt2Float = infix (TConv) ("toFloat", Nil, MFixPt::MFloat)	
+		impl (fixPt2Float) (codegen($cala, ${$0.toFloat})) 
+		val fixPt2Bool = infix (TConv) ("toBoolean", Nil, MFixPt::MBoolean)
+		impl (fixPt2Bool) (codegen($cala, ${$0 != 0})) 
+		val bool2FixPt = infix (TConv) ("toFixPt", Nil, MBoolean::MFixPt)
+		impl (bool2FixPt) (codegen($cala, ${if ($0) 1 else 0})) 
+
+		fimplicit (TConv) ("fixpt_to_float", Nil, MFixPt::MFloat) implements composite ${$0.toFloat}
+		fimplicit (TConv) ("float_to_fixpt", Nil, MFloat::MFixPt) implements composite ${$0.toFixPt}
+
 		()
 	}
 }

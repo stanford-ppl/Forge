508e447e66b62e45c76084413ff22886b2a38672
Yaqi Zhang <hsbzyq@gmail.com>
2016-01-13 01:04:20 -0800

    infix grp implementation of functions

--- src/dsls/dhdl/DHDL.scala
+++ src/dsls/dhdl/DHDL.scala
@@ -25,7 +25,6 @@ trait DHDLDSL extends ForgeApplication
 		lift(TpeOps) (MFloat)
 		lift(TpeOps) (MBoolean)
 
-		val FixPtOps = withTpe (FixPt)
 		def checkFixPtPrec (intPrec:Int, fracPrec:Int) = {
 			if (intPrec>=MAX_FIXPT_PRECISION)
 				throw new Exception("Integer precision cannot exceed " + (MAX_FIXPT_PRECISION-1) + ": " +
@@ -38,46 +37,44 @@ trait DHDLDSL extends ForgeApplication
 			}
 		}
 
-		FixPtOps {
-			infix ("getSign") (Nil :: MInt) implements signle ${
-				(SIGN_MASK & $0) >> (MAX_FIXPT_PRECISION-1)
-			}
-			/* getInt(intPrec, fracPrec): returns integer part of the fix point number */
-			infix ("getInt") ((MInt, MInt) :: MInt) implements single ${
-				val intPrec = $1
-				val fracPrec = $2
-				val intMask = (1L << intPrec + fracPrec) - 1 
-				(scala.math.abs($self) & intMask) >> fracPrec
-			}
-			/* getFrac(fracPrec): returns fraction part of the fix point number */
-			infix ("getFrac") (MInt :: MInt) implements single ${
-				val fracPrec = $1
-				val fracMask = (1L << fracPrec) - 1
-				(scala.math.abs($self) & fracMask)
-			}
-			direct ("FixPt") (((MInt,MInt,MInt,MInt,MInt) :: MInt)) implements codegen ${
-				val intPrec = $4
-				val fracPrec = $5
-				val sign = $1
-				val int = $2
-				val frac = $3
-				val posval = (int << fracPrec.toLong) | frac.toLong 
-				if (sign==0) posval else -posval
-			}
-			infix ("toString") ((MInt :: MInt) :: MString) implements single ${
-				if ($self.getSign==0) "" else "-" 
-				+ $self.getFrac($1, $2) + "." + frac
-			}
-			val fix2Float = infix ("toFloat") (Nil :: MFloat)
-			impl (fix2Float) (codegen($cala, ${ $0.toString.toFloat }))
+		infix (TpeOps) ("getSign", Nil, FixPt :: MInt) implements signle ${
+			(SIGN_MASK & $0) >> (MAX_FIXPT_PRECISION-1)
 		}
+		/* getInt(intPrec, fracPrec): returns integer part of the fix point number */
+		infix (TpeOps) ("getInt", Nil, (FixPt, MInt, MInt) :: MInt) implements single ${
+			val intPrec = $1
+			val fracPrec = $2
+			val intMask = (1L << intPrec + fracPrec) - 1 
+			(scala.math.abs($0) & intMask) >> fracPrec
+		}
+		/* getFrac(fracPrec): returns fraction part of the fix point number */
+		infix (TpeOps) ("getFrac", Nil, (FixPt, MInt) :: MInt) implements single ${
+			val fracPrec = $1
+			val fracMask = (1L << fracPrec) - 1
+			(scala.math.abs($0) & fracMask)
+		}
+		direct (TpeOps) ("FixPt", Nil, (FixPt,MInt,MInt,MInt,MInt,MInt) :: MInt) implements codegen ${
+			val intPrec = $4
+			val fracPrec = $5
+			val sign = $1
+			val int = $2
+			val frac = $3
+			val posval = (int << fracPrec.toLong) | frac.toLong 
+			if (sign==0) posval else -posval
+		}
+		infix (TpeOps) ("toString", Nil, (MInt :: MInt) :: MString) implements single ${
+			if ($0.getSign==0) "" else "-" 
+			+ $0.getFrac($1, $2) + "." + frac
+		}
+		val fix2Float = infix (TpeOps) ("toFloat", Nil, FixPt:: MFloat)
+		impl (fix2Float) (codegen($cala, ${ $0.toString.toFloat }))
 
-		val FloatOps = withTpe (MFloat)
-		FloatOps {
-			val float2fix = infix ("toFixPt") (Nil::FixPt)
-			//TODO: Don't know how to convert float to fixpt without knowing precision in FixPt
-			impl (float2fix) (codegen($cala, ${ FixPt($0.toInt, 0)} ))
-		}
+		//val FloatOps = withTpe (MFloat)
+		//FloatOps {
+		//	val float2fix = infix ("toFixPt") (Nil::FixPt)
+		//	//TODO: Don't know how to convert float to fixpt without knowing precision in FixPt
+		//	impl (float2fix) (codegen($cala, ${ FixPt($0.toInt, 0)} ))
+		//}
 
 		importDHDLPrimitives()
 		importMiscs()

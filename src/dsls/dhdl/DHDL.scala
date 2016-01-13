7b8c730be6897244c3f559f81dbf95fd09a29a52
Yaqi Zhang <hsbzyq@gmail.com>
2016-01-13 01:01:55 -0800

    withTpe implementation of functions

--- src/dsls/dhdl/DHDL.scala
+++ src/dsls/dhdl/DHDL.scala
@@ -8,6 +8,9 @@ object DHDLDSLRunner extends ForgeApplicationRunner with DHDLDSL
 
 trait DHDLDSL extends ForgeApplication 
 	with PrimOps with MiscOps{
+
+	val MAX_FIXPT_PRECISION = 64
+	val SIGN_MASK:Long = 1L << (MAX_FIXPT_PRECISION-1)
 	
   def dslName = "DHDL"
 	
@@ -15,28 +18,85 @@ trait DHDLDSL extends ForgeApplication
 
   def specification() = {
 
-		val MFixPt = tpe("Int", stage=future)
+		val FixPt = tpe("Long", stage=future)
+
+		val TpeOps = grp("TpeOps")
+		lift(TpeOps) (MInt)
+		lift(TpeOps) (MFloat)
+		lift(TpeOps) (MBoolean)
+
+		val FixPtOps = withTpe (FixPt)
+		def checkFixPtPrec (intPrec:Int, fracPrec:Int) = {
+			if (intPrec>=MAX_FIXPT_PRECISION)
+				throw new Exception("Integer precision cannot exceed " + (MAX_FIXPT_PRECISION-1) + ": " +
+					intPrec)
+			if (fracPrec>=MAX_FIXPT_PRECISION)
+				throw new Exception("Fraction precision cannot exceed " + (MAX_FIXPT_PRECISION-1) + ": " + fracPrec)
+			if ((intPrec + fracPrec)>=MAX_FIXPT_PRECISION) {
+				throw new Exception("Sum of integer and fraction precisions cannot exceed "
+					(MAX_FIXPT_PRECISION-1) + ": " + (intPrec + fracPrec))
+			}
+		}
+
+		FixPtOps {
+			infix ("getSign") (Nil :: MInt) implements signle ${
+				(SIGN_MASK & $0) >> (MAX_FIXPT_PRECISION-1)
+			}
+			/* getInt(intPrec, fracPrec): returns integer part of the fix point number */
+			infix ("getInt") ((MInt, MInt) :: MInt) implements single ${
+				val intPrec = $1
+				val fracPrec = $2
+				val intMask = (1L << intPrec + fracPrec) - 1 
+				(scala.math.abs($self) & intMask) >> fracPrec
+			}
+			/* getFrac(fracPrec): returns fraction part of the fix point number */
+			infix ("getFrac") (MInt :: MInt) implements single ${
+				val fracPrec = $1
+				val fracMask = (1L << fracPrec) - 1
+				(scala.math.abs($self) & fracMask)
+			}
+			direct ("FixPt") (((MInt,MInt,MInt,MInt,MInt) :: MInt)) implements codegen ${
+				val intPrec = $4
+				val fracPrec = $5
+				val sign = $1
+				val int = $2
+				val frac = $3
+				val posval = (int << fracPrec.toLong) | frac.toLong 
+				if (sign==0) posval else -posval
+			}
+			infix ("toString") ((MInt :: MInt) :: MString) implements single ${
+				if ($self.getSign==0) "" else "-" 
+				+ $self.getFrac($1, $2) + "." + frac
+			}
+			val fix2Float = infix ("toFloat") (Nil :: MFloat)
+			impl (fix2Float) (codegen($cala, ${ $0.toString.toFloat }))
+		}
+
+		val FloatOps = withTpe (MFloat)
+		FloatOps {
+			val float2fix = infix ("toFixPt") (Nil::FixPt)
+			//TODO: Don't know how to convert float to fixpt without knowing precision in FixPt
+			impl (float2fix) (codegen($cala, ${ FixPt($0.toInt, 0)} ))
+		}
 
 		importDHDLPrimitives()
 		importMiscs()
 
-		val Prim = lookupGrp("DHDLPrim")
-		lift(Prim) (MFloat)
-		lift(Prim) (MFixPt)
-		lift(Prim) (MBoolean)
+		//val Prim = lookupGrp("DHDLPrim")
 
-		val TConv = grp("TypeConv") 
-		val float2FixPt = infix (TConv) ("toFixPt", Nil, MFloat::MFixPt)	
-		impl (float2FixPt) (codegen($cala, ${$0.toInt})) 
-		val fixPt2Float = infix (TConv) ("toFloat", Nil, MFixPt::MFloat)	
-		impl (fixPt2Float) (codegen($cala, ${$0.toFloat})) 
-		val fixPt2Bool = infix (TConv) ("toBoolean", Nil, MFixPt::MBoolean)
-		impl (fixPt2Bool) (codegen($cala, ${$0 != 0})) 
-		val bool2FixPt = infix (TConv) ("toFixPt", Nil, MBoolean::MFixPt)
-		impl (bool2FixPt) (codegen($cala, ${if ($0) 1 else 0})) 
 
-		fimplicit (TConv) ("fixpt_to_float", Nil, MFixPt::MFloat) implements composite ${$0.toFloat}
-		fimplicit (TConv) ("float_to_fixpt", Nil, MFloat::MFixPt) implements composite ${$0.toFixPt}
+		//val TConv = grp("TypeConv") 
+		//val float2FixPt = infix (TConv) ("toFixPt", Nil, MFloat::MFixPt)	
+		//impl (float2FixPt) (codegen($cala, ${$0.toInt})) 
+		//val fixPt2Float = infix (TConv) ("toFloat", Nil, MFixPt::MFloat)	
+		//impl (fixPt2Float) (codegen($cala, ${$0.toFloat})) 
+		//val fixPt2Bool = infix (TConv) ("toBoolean", Nil, MFixPt::MBoolean)
+		//impl (fixPt2Bool) (codegen($cala, ${$0 != 0})) 
+		//val bool2FixPt = infix (TConv) ("toFixPt", Nil, MBoolean::MFixPt)
+		//impl (bool2FixPt) (codegen($cala, ${if ($0) 1 else 0})) 
+
+		//fimplicit (TConv) ("fixpt_to_float", Nil, MFixPt::MFloat) implements composite ${$0.toFloat}
+		//fimplicit (TConv) ("float_to_fixpt", Nil, MFloat::MFixPt) implements composite ${$0.toFixPt}
 
 		()
 	}

2e0cfaaf3644158867ccb50026bb8688f51c337f
raghu <raghup17@gmail.com>
2016-02-14 20:18:21 -0800

    Changes to add an ALU and Link node. Each new node is added as a new tpe, and hence has its own static apply method. However, in order to have greater codegen control at the DSL spec level, the apply method directly generates code instead of implementing an 'allocates' method.
    For this to work, the generated code for scala backend must instantiate
    an object of the type created. Currently the Scala backend instantiates
    anonymous class objects of the types created, which means that the
    generated code basically re-creates the IR. This is a temporary solution
    because it makes the Scala backend useless. A permanent solution is to
    statically add a template for each node implemented as a codegen node in
    the apply method, and have the Scala backend instantiate that template
    as the codegen rule.

--- src/dsls/dadl/ArchOps.scala
+++ src/dsls/dadl/ArchOps.scala
@@ -1,7 +1,7 @@
 package ppl.dsl.forge
 package dsls
 package dadl
-
+/*
 trait ArchOps {
 	this: DADLDSL =>
 	def importDADLArchOps() = {
@@ -12,28 +12,26 @@ trait ArchOps {
 
 		val BRAM = tpe("BRAM", tpePar("T"))
 		data(BRAM, ("_name", MString),
-//      ("_wordWidth", MInt),
-//      ("_banks", MInt),
+      ("_banks", MInt),
       ("_data", MArray(T)))
 
     // To instantiate BRAM, support:
     // BRAM(name: String, banks: Int, wordWidth: Int, size: Int)
 		static (BRAM) ("apply", T,
 			MethodSignature(List( ("name", MString, "unit(\"\")"),
-//                            ("wordWidth", MInt),
-//                            ("banks", MInt),
+                            ("banks", MInt),
                             ("size", MInt)
-                          ),BRAM(T)),
+                          ), MString),
 			effect = mutable) implements
-		allocates(BRAM, ${$name}, ${array_empty[T]($size)})
+		codegen ($cala, ${"codegen for BRAM( " + $name + ", " + $banks + "," + $size +")"})
 
 		val BRAMOps = withTpe(BRAM)
 		BRAMOps {
 			infix ("name") (Nil :: MString) implements getter(0, "_name")
-//			infix ("wordWidth") (Nil :: MInt) implements getter(0, "_wordWidth")
-//			infix ("banks") (Nil :: MInt) implements getter(0, "_banks")
+			infix ("banks") (Nil :: MInt) implements getter(0, "_banks")
 			infix ("data") (Nil :: MArray(T)) implements getter(0, "_data")
 		}
 	}
 }
+*/
 
--- src/dsls/dadl/DADL.scala
+++ src/dsls/dadl/DADL.scala
@@ -7,29 +7,74 @@ import core.{ForgeApplication,ForgeApplicationRunner}
 object DADLDSLRunner extends ForgeApplicationRunner with DADLDSL
 
 trait DADLDSL extends ForgeApplication
-  with ArchOps with Modules {
+  with ArchOps
+  with NodeOps {
+//  with ArchOps {
+//  with Modules {
 
   def dslName = "DADL"
 
-  override def addREPLOverride = false
+//  override def addREPLOverride = false
+  def testing() = {
+    val DirectOps = grp("Direct")
+    val myprintlnOp = direct (DirectOps) (
+      name = "myprintln",
+      tpePars = List(),
+      MAny :: MUnit,
+      effect = simple)
 
-  def specification() = {
-		importDADLArchOps()
-    importTuples()
-    importModules()
+    impl (myprintlnOp) { codegen ($cala, ${println($0)}) }
+    impl (myprintlnOp) { codegen (cpp, ${ std::cout << $0 << std::endl }) }
+
+    /**
+     * Example composite op
+     */
+		lift(DirectOps) (MInt)
+		lift(DirectOps) (MBoolean)
+		importStrings()
+    importMisc()
+    val IntOps = withTpe (MInt)
+		IntOps {
+			infix ("+") (MInt :: MInt) implements codegen ($cala, ${ $self + $1 })
+		  infix ("<") (MInt :: MBoolean) implements codegen ($cala, ${ $self < $1 })
+		}
 
     val T = tpePar("T")
-    val rep = tpe("Rep", T, stage=compile)
-    primitiveTypes ::= rep
-    tpeAlias("Wire", rep)
+    val printLT = direct (DirectOps) (
+      name = "printLT",
+      tpePars = List(T),
+      MethodSignature(List(("arr", MArray(MInt)), ("maxnum", MInt)), MUnit),
+      effect = simple)
 
-		val TypeOps = grp("TypeOps")
-		lift(TypeOps) (MInt)
-		lift(TypeOps) (MArray)
-    direct (TypeOps) ("println", List(), List(MAny) :: MUnit, effect = simple) implements codegen($cala, ${ println($0) })
+    impl (printLT) { composite ${
+      var i = 0
+      while(i < $arr.length) {
+        if ($arr(i) < $maxnum) println(i)
+        i = i + 1
+      }
+    }}
+  }
 
-		importStrings()
+  def otherStuff() = {
+//		importDADLArchOps()
 
+//    importTuples()
+//    importModules()
+//    importDADLDirectOps()
+
+//    val T = tpePar("T")
+//    val rep = tpe("Rep", T, stage=compile)
+//    primitiveTypes ::= rep
+//    tpeAlias("Wire", rep)
+
+//		val TypeOps = grp("TypeOps")
+//		lift(TypeOps) (MArray)
+/*    direct (TypeOps) ("println", List(), List(MAny) :: MUnit, effect = simple) implements codegen($cala, ${ println($0) }) */
+  }
+
+  def specification() = {
+    importCore()
+    importNodes()
     ()
 	}
 }

0ef7b51d327cd517c1323541bd8b1d67595d4d4a
raghu <raghup17@gmail.com>
2016-05-12 16:18:40 -0700

    Prepend cout and endl with std:: for println codegen

--- src/dsls/dhdl/DHDLMisc.scala
+++ src/dsls/dhdl/DHDLMisc.scala
@@ -306,9 +306,8 @@ trait DHDLMisc {
     // C++ backend
     impl (set_arg)  (codegen(cpp, ${ *$0 = $1}))
     impl (get_arg)  (codegen(cpp, ${ *$0 }))
-    impl (println)  (codegen(cpp, ${ cout << $0 << endl }))
-    impl (println2) (codegen(cpp, ${ cout << endl }))
-
+    impl (println)  (codegen(cpp, ${ std::cout << $0 << std::endl }))
+    impl (println2) (codegen(cpp, ${ std::cout << std::endl }))
     // --- Dot Backend
     impl (println)  (codegen(dot, ${
 			@ emitComment("println")

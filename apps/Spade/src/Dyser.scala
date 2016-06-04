0b3fbb17fcf030b3c758ce655f71cccafb98708e
raghu <raghup17@gmail.com>
2016-06-03 20:20:51 -0700

    Parameterize Dyser

--- apps/Spade/src/Dyser.scala
+++ apps/Spade/src/Dyser.scala
@@ -7,55 +7,35 @@ object DyserCompiler extends SpadeApplicationCompiler with Dyser
 object DyserInterpreter extends SpadeApplicationInterpreter with Dyser
 
 trait Dyser extends SpadeApplication {
+  def dyser(rows: Int, cols: Int)  {
+    val switches = ListBuffer[ListBuffer[Rep[Switch]]]()
+    for (i <- 0 to rows) {
+      val switchRow = ListBuffer[Rep[Switch]]()
+      for (j <- 0 to cols) {
+        switchRow.append(Switch())
+        if (j > 0) switchRow(j-1) <-> switchRow(j)
+        if (i > 0) switches(i-1)(j) <-> switchRow(j)
+      }
+      switches.append(switchRow)
+      if (i > 0) {
+        for (k <- 0 until cols) {
+          val a = ALU()
+          a <-> switches(i-1)(k)
+          a <-> switches(i-1)(k+1)
+          a <-> switches(i)(k)
+          a <-> switches(i)(k+1)
+        }
+      }
+    }
+  }
+
   def main() = {
-    val a00 = ALU()
-    val s00 = Switch()
-    val s01 = Switch()
-    val s10 = Switch()
-    val s11 = Switch()
-    val a01 = ALU()
-    val s02 = Switch()
-    val s12 = Switch()
-    val a10 = ALU()
-    val a11 = ALU()
-    val s20 = Switch()
-    val s21 = Switch()
-    val s22 = Switch()
-
-    s00 <-> s01
-    s00 <-> s10
-    s01 <-> s02
-    s01 <-> s11
-    s10 <-> s11
-    s11 <-> s12
-    s02 <-> s12
-
-    s10 <-> s20
-    s11 <-> s21
-    s12 <-> s22
-    s20 <-> s21
-    s21 <-> s22
-
-
-    a00 <-> s00
-    a00 <-> s01
-    a00 <-> s10
-    a00 <-> s11
-
-    a01 <-> s01
-    a01 <-> s02
-    a01 <-> s11
-    a01 <-> s12
-
-    a10 <-> s10
-    a10 <-> s11
-    a10 <-> s20
-    a10 <-> s21
-
-    a11 <-> s11
-    a11 <-> s12
-    a11 <-> s21
-    a11 <-> s22
-
+    if (stagingArgs.length < 2) {
+      println("Usage: DyserCompiler <rows> <cols>")
+      sys.exit(-1)
+    }
+    val rows = stagingArgs(0).toInt
+    val cols = stagingArgs(1).toInt
+    dyser(rows, cols)
   }
 }

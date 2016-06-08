4d09920c3f4f1ba481d5aaaec46c2f48d654543e
Yaqi Zhang <hsbzyq@gmail.com>
2016-06-08 13:36:29 -0700

    fixes to BFS. Not supported by dhdl yet due to doesn't support using function of register value as range of tile load

--- apps/DHDL/src/BFS.scala
+++ apps/DHDL/src/BFS.scala
@@ -9,8 +9,11 @@ trait BFS extends DHDLApplication {
   override def stageArgNames = List("tileSize")
   lazy val tileSize = param("tileSize", 2)
   lazy val maxNumEdge = 8.as[Index]
+  lazy val diameter = ArgIn[SInt]("diameter")
+  lazy val numVert = ArgIn[SInt]("numVert")
 
   // Limitation:
+  // Not using bitMask due to filter is not supported
   def main() {
     //val NV = args(unit(0)).to[SInt]
     ////genRandDirEdgeList("/Users/Yaqi/Documents/hyperdsl/published/DHDL/graph.dot", NV, NE, true)
@@ -20,7 +23,7 @@ trait BFS extends DHDLApplication {
     val el = getEdgeList(map) // Flat edge list
     val NV = vl.length/2 // Actual number of vertices in graph
     val NE = el.length
-    val DM = 10 // Diameter
+    val DM = 10.as[SInt] // Diameter
 
     println("vl:" + vl.mkString(","))
     println("el:" + el.mkString(","))
@@ -29,50 +32,60 @@ trait BFS extends DHDLApplication {
     val edgeList = OffChipMem[Index]("EdgeList", NE) // srcs of edges
     val distList = OffChipMem[Index]("DistList", NV) // srcs of edges
 
-    setMem(vertList, vl)
-    setMem(edgeList, el)
-
-    Accel {
-      //val bitMap = BRAM[Bit]("bitMap", NV) // Assume reset to 0
-      //Sequential (DM by 1) { level =>
-        Pipe (NV by tileSize) { ivt =>
-          val vB = BRAM[Index]("vB", tileSize, 2) 
-          //val distB = BRAM[Index]("distB", tileSize) 
-          vB := vertList(ivt::ivt+tileSize, 0::2)
-          //distB := distList(ivt::ivt+tileSize)
-          Pipe (tileSize by 1) {iv =>
-            //val eB = BRAM[Index]("edgeTile", maxNumEdge) // edge list of v
-            //val eDistB = BRAM[Index]("edgeDistTile", maxNumEdge) // dists of edge list of v
-            val vpt = Reg[Index]("vpt") // ptr to v's edgelist
-            val vsize = Reg[Index]("vsize") // number of edges of v
-            Pipe {
-              vpt := vB(iv,0)
-              vsize := vB(iv,1)
-            }
-            //eB := edgeList(vpt::vpt+vsize.value)
-            //eDistB := distList(eB, vsize)
-            //Pipe (vsize.value by 1) { ie =>
-            //  //val nbr = eB(ie)
-            //  //val isCurLevel = (distB(iv) == level)
-            //  //eDistB(ie) = mux(isCurLevel, eDistB(ie)+1, eDistB(ie))
-            //}
-            //distList(eB) := eDistB
-          }
-        }
-      //}
-      ()
-    }
-
-    /* Scala Version */
+    // Initialize dist to -1 except the source
     val gold = Array.empty[SInt](NV)
     for (i <- 0 until NV) {
       gold(i) = -1.as[SInt]
     }
     gold(0) = 0.as[SInt]
 
-    val diameter = 10.as[SInt]
+    setMem(vertList, vl)
+    setMem(edgeList, el)
+    setMem(distList, gold)
 
-    for (level <- 0 until diameter) {
+    setArg(diameter, DM)
+    setArg(numVert, NV)
+
+    Accel {
+      //val bitMap = BRAM[Bit]("bitMap", NV) // Assume reset to 0
+      Sequential (diameter.value by 1) { level =>
+        Pipe (numVert.value by tileSize) { ivt =>
+          val vB = BRAM[Index]("vB", tileSize, 2) 
+          val distB = BRAM[Index]("distB", tileSize) 
+          vB := vertList(ivt::ivt+tileSize, 0::2)
+          distB := distList(ivt::ivt+tileSize)
+          Pipe (tileSize by 1) {iv =>
+            val eB = BRAM[Index]("edgeTile", maxNumEdge) // edge list of v
+            val eDistB = BRAM[Index]("edgeDistTile", maxNumEdge) // dists of edge list of v
+            val vpt = Reg[Index]("vpt") // ptr to v's edgelist
+            val vsize = Reg[Index]("vsize") // number of edges of v
+            Pipe {
+              vpt := vB(iv,0)
+              vsize := vB(iv,1)
+            }
+            Parallel { 
+              //TODO: doesn't support using function of register value as range of tile load
+              eB := edgeList(vpt.value::vpt.value+vsize.value)
+              eDistB := distList(eB, vpt.value+vsize.value)
+            }
+            Pipe { //TODO: has to wrap the innerpipe inside another pipe to make use of vsize not in parent of Pipe where it's created
+              Pipe (vsize.value by 1) { ie =>
+                val nbr = eB(ie)
+                val isCurLevel = (distB(iv) == level)
+                val isNbrNotSet = (distB(nbr) < 0.as[SInt])
+                eDistB(ie) = mux(isCurLevel && isNbrNotSet, level+1, eDistB(ie))
+              }
+            }
+            distList(eB) := eDistB
+          }
+        }
+      }
+      ()
+    }
+
+    /* Scala Version */
+
+    for (level <- 0 until DM) {
       println("level: " + level)
       for (iv <- 0 until NV) {
         val pt = vl(iv * 2)
@@ -81,7 +94,8 @@ trait BFS extends DHDLApplication {
         if (gold(iv)==level) {
           for (ie <- 0 until size) {
             val e = el(pt + ie)
-            gold(e) = level + 1
+            if (gold(e)>0)
+              gold(e) = level + 1
             //println("ie: " + ie + " e:" + e)
             //println("gold(e):" + gold(e))
             //println("gold:" + gold.mkString(","))

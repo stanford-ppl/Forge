e235f0da4545866912f2d94ebcc61f11ff7b386a
raghu <raghup17@gmail.com>
2016-06-09 19:26:44 -0700

    Really implement regs in C++

--- src/dsls/dhdl/DHDLRegs.scala
+++ src/dsls/dhdl/DHDLRegs.scala
@@ -113,9 +113,12 @@ trait DHDLRegs {
       @ val tpname = remap(sym.tp.typeArguments(0))
       new $tpname {$init}
     }))
-    impl (reg_read)  (codegen(cpp, ${ <todo_reg_read> $reg.apply(0) }))
-    impl (reg_write) (codegen(cpp, ${ <todo_reg_write> $reg.update(0, $value) }))
-    impl (reg_reset) (codegen(cpp, ${ <todo_reg_reset> }))
+    impl (reg_read)  (codegen(cpp, ${ *$reg }))
+    impl (reg_write)  (codegen(cpp, ${ *$reg = $value}))
+    impl (reg_reset) (codegen(cpp, ${
+      @ val init = resetValue($reg)
+      *$reg = $init
+    }))
 
     // --- MaxJ Backend
     //reg_new (extern)

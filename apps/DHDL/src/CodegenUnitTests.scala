63f4e6a3fe4d2b2d57d8a1aa99491eceaff1b281
raghu <raghup17@gmail.com>
2016-07-24 21:28:31 -0700

    Move SimpleReduce to codegen unit tests, add more for FIFO -> BRAM interface. Currently, using a single FIFO between tile load and store will not work.

--- apps/DHDL/src/CodegenUnitTests.scala
+++ apps/DHDL/src/CodegenUnitTests.scala
@@ -144,3 +144,127 @@ trait SimpleTileLoadStore extends DHDLApplication {
   }
 }
 
+object FifoLoadTest extends DHDLApplicationCompiler with FifoLoad
+trait FifoLoad extends DHDLApplication {
+  type T = SInt
+  type Array[T] = ForgeArray[T]
+  def fifoLoad(srcHost: Rep[Array[T]]) = {
+    val tileSize = param("tileSize", 96); domainOf(tileSize) = (96, 96, 96)
+    val N = srcHost.length; bound(N) = 9216
+
+    val srcFPGA = OffChipMem[SInt](N)
+    val dstFPGA = OffChipMem[SInt](N)
+    setMem(srcFPGA, srcHost)
+
+    Accel {
+      val f1 = FIFO[SInt](tileSize)
+      Sequential {
+        f1 := srcFPGA(0::tileSize)
+        val b1 = BRAM[SInt](tileSize)
+        Pipe(tileSize by 1) { i =>
+          b1(i) = f1.pop
+        }
+        dstFPGA(0::tileSize) := b1
+      }
+      ()
+    }
+    getMem(dstFPGA)
+  }
+
+  def main() {
+    val arraySize = args(unit(0)).to[SInt]
+
+    val src = Array.tabulate[SInt](arraySize) { i => i }
+    val dst = fifoLoad(src)
+
+    val gold = src.map { i => i }
+
+    println("dst:")
+    (0 until arraySize) foreach { i => print(dst(i) + " ") }
+    println("")
+
+    (0 until arraySize) foreach { i => assert(dst(i) == gold(i)) }
+  }
+}
+
+
+object FifoLoadStoreTest extends DHDLApplicationCompiler with FifoLoadStore
+trait FifoLoadStore extends DHDLApplication {
+  type T = SInt
+  type Array[T] = ForgeArray[T]
+  def fifoLoadStore(srcHost: Rep[Array[T]]) = {
+    val tileSize = param("tileSize", 96); domainOf(tileSize) = (96, 96, 96)
+    val N = srcHost.length; bound(N) = 9216
+
+    val srcFPGA = OffChipMem[SInt](N)
+    val dstFPGA = OffChipMem[SInt](N)
+    setMem(srcFPGA, srcHost)
+
+    Accel {
+      val f1 = FIFO[SInt](tileSize)
+      Parallel {
+        Sequential {
+          f1 := srcFPGA(0::tileSize)
+          dstFPGA(0::tileSize) := f1
+        }
+        Pipe(tileSize by 1) { i => }
+      }
+      ()
+    }
+    getMem(dstFPGA)
+  }
+
+  def main() {
+    val arraySize = args(unit(0)).to[SInt]
+
+    val src = Array.tabulate[SInt](arraySize) { i => i }
+    val dst = fifoLoadStore(src)
+
+    val gold = src.map { i => i }
+
+    println("dst:")
+    (0 until arraySize) foreach { i => print(dst(i) + " ") }
+    println("")
+
+    (0 until arraySize) foreach { i => assert(dst(i) == gold(i)) }
+  }
+}
+
+object SimpleReduceTest extends DHDLApplicationCompiler with SimpleReduce
+trait SimpleReduce extends DHDLApplication {
+  type T = SInt
+  type Array[T] = ForgeArray[T]
+  val constTileSize = 96
+  def simplemap(xin: Rep[SInt]) = {
+    val innerPar = param("innerPar", 1); domainOf(innerPar) = (1, 1, 1)
+    val tileSize = param("tileSize", constTileSize); domainOf(constTileSize) = (constTileSize, constTileSize, constTileSize)
+
+    val x = ArgIn[SInt]
+    val out = ArgOut[SInt]
+    setArg(x, xin)
+
+    Accel {
+      Sequential {
+        val accum = Reduce (constTileSize par innerPar)(0.as[T]) { ii =>
+          x.value * ii
+        } {_+_}
+        Pipe { out := accum }
+      }
+      ()
+    }
+
+    getArg(out)
+  }
+
+  def main() {
+    val x = args(unit(0)).to[SInt]
+
+    val result = simplemap(x)
+
+    val b1 = Array.tabulate[SInt](constTileSize) { i => x * i }
+    val gold = b1.reduce {_+_}
+    println("expected: " + gold)
+    println("result:   " + result)
+    assert(result == gold)
+  }
+}
